3abf9ed2fc26d9ee1aa5555a8e43338dc100006b
Carlos-Larin <nekokar701@gmail.com>
2025-03-15 01:31:03 -0600

    borado

--- classSemana8/login.php
+++
@@ -1,7 +0,0 @@
-<form action="iniciar_secion.php" method="post">
-    <label for="">Escribe tu nombre</label>
-    <input type="text" id="" name="user">
-    <label for="">Contrasena</label>
-    <input type="password" id="" name="pass">
-    <button type="submit">Iniciar sesion</button>
-</form>
